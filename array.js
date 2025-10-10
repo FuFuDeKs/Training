7b02a0dc5740b654352c831da222cefb371d3093
testuser <test@test.test>
2025-10-10 11:27:38 +0300

    2-nd commit

--- array.js
+++ array.js
@@ -1,12 +1,5 @@
 const colors = ["NAME1", "NAME2", "NAME3"];
-colors[4] = "NAME4";
+colors[3] = "NAME4";
 colors.forEach((item, index) => {
   console.log(`${index}: ${item}`);
 });
-// Output:
-// 0: red
-// 1: yellow
-// 2: blue
-// 5: purple
-
-colors.reverse(); // ['purple', empty × 2, 'blue', 'yellow', 'red']
